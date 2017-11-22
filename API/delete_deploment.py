5b1a8733e65bb1dad31fe340213531d007f25dc2
cynthia <ks712@hotmail.com>
2017-11-22 19:07:20 +0800

    init api

---
+++ API/delete_deploment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import yml
+from kubernetes import client, config
+import json
+
+def delete_deployment(api_instance):
+    # Delete deployment
+    api_response = api_instance.delete_namespaced_deployment(
+        name=DEPLOYMENT_NAME,
+        namespace="default",
+        body=client.V1DeleteOptions(
+            propagation_policy='Foreground',
+            grace_period_seconds=5))
+    print("Deployment deleted. status='%s'" % str(api_response.status))
---
+++ API/update_deployment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import yml
+from kubernetes import client, config
+import json
+
+def update_deployment(api_instance, deployment):
+    # Update container image
+    deployment.spec.template.spec.containers[0].image = "nginx:1.9.1"
+    # Update the deployment
+    api_response = api_instance.patch_namespaced_deployment(
+        name=DEPLOYMENT_NAME,
+        namespace="default",
+        body=deployment)
+    print("Deployment updated. status='%s'" % str(api_response.status))
